f0286098eb189bdaa664cd850d3023890e1cbaa3
Thomas Eding <thomaseding@gmail.com>
2022-03-19 19:59:22 -0700

    wip assets

---
+++ src/ts/assets.ts
@@ -0,0 +1,17 @@
+
+class Assets {
+    private constructor() {
+        this._cache = {};
+    }
+
+    public getImageData(path: string): ImageData {
+        const image = this._cache[path];
+        if (image instanceof ImageData) {
+            return image;
+        }
+    }
+
+    private static readonly _this = new Assets();
+    private readonly _cache: Record<string, object>;
+}
+
